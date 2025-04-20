4fbb3bb905136920ac1a6c8c77ca1008406246dc
elshangurbanow <elsenqurbanov50@gmail.com>
2025-04-20 21:57:17 +0000

    chanceget

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 "use strict";
 $(document).ready(function () {
+    
+});
 
+$('h2').click(function (e) {
+    e.preventDefault();
+    $('h2').html(1234);
+});
+
+$('.btn').click(function (e) {
+    e.preventDefault();
+    $('input').html(e * 5);
 });
