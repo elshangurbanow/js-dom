1d0a080a105ef51c0b0814653b802debb2059920
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-02 20:49:39 +0000

    commit

--- app.js
+++ app.js
@@ -3,19 +3,4 @@ $(document).ready(function () {
     
 });
 
-$('.btn').click(function (e) {
-    e.preventDefault();
-
-    let a = (a) => {
-        if (a < 0) {
-            console.log(a($('input').val()))
-        } else{
-            for (let i = 1; i <=a; i++) {
-                $('.col-md-12').append(`<h1>${a}<h1>`);
-            }
-        }
-        
-    }
-    a($('input').val())
-});
 
