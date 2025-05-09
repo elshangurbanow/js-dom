33566817577441a549d4f574c54c3e19564ddc69
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-09 18:36:26 +0000

    commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 "use strict";
 $(document).ready(function () {
 
-    $('#demojs').className('#box') 
+
+    $('.col-md-12').append('<div class="box"></div>');
+    $('.box').val('h1');
+
+
 });
 
 
+   //d-flex justify-content-center
