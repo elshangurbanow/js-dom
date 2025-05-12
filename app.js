b0db011f97a21d79a8e7c7c5caa005c80dad2953
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-12 17:20:07 +0000

    commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-"use strict";
 $(document).ready(function () {
 
 
 
-    $('.btn').click( function (e) {
-        
-    });
+    $('.btn').click(function (e) {
+    e.preventDefault();
+    if($.trim($('input').val()) == '') {
+        $('input').val();
+    } else {
+        let k = v => {
+            for (i = 0; i < v; i++) {$('#answer').append(`<div class="minibox"><div class="btn"><h5>${v}</h5></div></div>`)};
+        }
+        let z = e => Number(e) ? k(e) : $('#answer').append(`<div class="minibox"><div class="btn"><h5>${e}</h5></div></div>`);
+        z($('input').val());
+    }
+    $('input').val('')
+});
 
 
 
